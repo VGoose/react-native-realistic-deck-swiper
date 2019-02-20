f12cb7ee81f84f1daa2f0243f887219f8534cf7f
AnhVoMBP15 <anhvouw@gmail.com>
2019-02-19 21:57:28 -0500

    2/19_2

--- example/App.js
+++ example/App.js
@@ -1,245 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
-// import math from 'mathjs'
+import { View } from 'react-native';
+
 import Swiper from './swiper'
-import Foo from './foo'
 
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const SCREEN_WIDTH = Dimensions.get('window').width
-
-const Users = [
-  { id: "1", desc: 'icecream', uri: require('./assets/1.jpg') },
-  { id: "2", desc: 'soup', uri: require('./assets/2.jpg') },
-  { id: "3", desc: 'popcorn', uri: require('./assets/3.jpg') },
-  { id: "4", desc: 'popcorn+', uri: require('./assets/4.jpg') },
-  { id: "5", desc: 'popcorn++', uri: require('./assets/5.jpg') },
+const Data = [
+  { id: "1" },
+  { id: "2" },
+  { id: "3" },
+  { id: "4" },
+  { id: "5" },
 ]
-
 export default class App extends React.Component {
-
-  constructor() {
-    super()
-
-    this.position = new Animated.ValueXY()
-    this.rotation = new Animated.Value()
-    this.state = {
-      currentIndex: 0,
-      eventX: 0,
-      eventY: 0,
-      gestureX0: 0,
-      gestureY0: 0,
-      vx: 0,
-      vy: 0,
-      gestureDX: 0,
-      gestureDY: 0,
-      normalizedRadius: 0,
-      clockwise: null,
-      result: null,
-      resultRelease: null,
-    }
-
-    this.rotate = this.rotation.interpolate({
-      inputRange: [-100000, 0, 100000],
-      outputRange: ['-150deg', '0deg', '150deg'],
-      // extrapolate: 'clamp'
-    })
-
-    this.rotateAndTranslate = {
-      transform: [
-        ...this.position.getTranslateTransform(),
-        {
-          rotate: this.rotate
-        },
-      ]
-    }
-
-    this.likeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp'
-    })
-    this.dislikeOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0, 0],
-      extrapolate: 'clamp'
-    })
-
-    this.nextCardOpacity = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0, 1],
-      extrapolate: 'clamp'
-    })
-    this.nextCardScale = this.position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
-      outputRange: [1, 0.8, 1],
-      extrapolate: 'clamp'
-    })
-
-  }
   _renderCard = (item) => {
     return <View style={{ width: 300, height: 400, borderRadius: 5 }}>
     </View>
   }
-  componentWillMount() {
-    this.PanResponder = PanResponder.create({
-
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderMove: (evt, gestureState) => {
-
-        const { moveX, moveY, x0, y0, dx, dy, vx, vy, } = gestureState
-
-        const originX = 1 / 2 * (250) + (SCREEN_WIDTH - 250) / 2
-        const originY = 1 / 2 * (400) + 70 //offset from top
-        let x = moveX - originX
-        let y = moveY - originY
-        // let crossProduct = math.cross([x, y, 0], [dx, dy, 0])
-        let rotation = x * dy - y * dx
-        // let result = crossProduct[2]
-
-        Animated.event([null, { dx: this.position.x, dy: this.position.y }])(null, gestureState)
-        this.rotation.setValue(rotation)
-      },
-      onPanResponderRelease: (evt, gestureState) => {
-        const { moveX, moveY, dx, dy, vx, vy } = gestureState
-        const originX = 1 / 2 * (250) + (SCREEN_WIDTH - 250) / 2
-        const originY = 1 / 2 * (400) + 70 //offset from top
-        let x = moveX - originX
-        let y = moveY - originY
-        // let crossProduct = math.cross([x, y, 0], [dx, dy, 0])
-        // let resultRelease = crossProduct[2]
-        let rotation0 = x * dy - y * dx
-        // let intCrossProduct = math.cross([x, y, 0], [vx * 1000, vy * 1000, 0])
-        // let interpolation = intCrossProduct[2]
-        let rotation1000 = x * vy * 1000 - y * vx * 1000
-        this.setState({ vx, vy, resultRelease })
-        const vMagnitude = Math.sqrt(vx * vx + vy * vy)
-        if (vMagnitude > .4) {
-          Animated.parallel([
-            Animated.timing(this.position, {
-              toValue: { x: vx * 1000, y: vy * 1000 },
-              duration: 1000
-            }),
-            Animated.timing(this.rotation, {
-              toValue: rotation0 + rotation1000,
-              duration: 1000
-            })
-          ]
-          ).start(() => {
-            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-              this.rotation.setValue(0)
-            })
-          })
-        }
-        else if (gestureState.vx < -0.8) {
-          Animated.spring(this.position, {
-            toValue: { x: -SCREEN_WIDTH - 100, y: gestureState.dy }
-          }).start(() => {
-            this.setState({ currentIndex: this.state.currentIndex + 1 }, () => {
-              this.position.setValue({ x: 0, y: 0 })
-            })
-          })
-        }
-        else {
-          Animated.spring(this.rotation, {
-            toValue: 0,
-            stiffness: 50,
-            damping: 30,
-            mass: 0.5
-          }).start()
-          Animated.spring(this.position, {
-            toValue: { x: 0, y: 0 },
-            friction: 4
-          }).start()
-        }
-      }
-    })
-  }
-
-  renderUsers = () => {
-
-    return Users.map((item, i) => {
-
-
-      if (i < this.state.currentIndex) {
-        return null
-      }
-      else if (i == this.state.currentIndex) {
-
-        return (
-          <Animated.View
-            {...this.PanResponder.panHandlers}
-            key={item.id} style={
-              [this.rotateAndTranslate,
-              {
-                height: 400,
-                width: 250, padding: 10,
-                position: 'absolute'
-              }]
-            }>
-            <Animated.View style={{ opacity: this.likeOpacity, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
-
-            </Animated.View>
-
-            <Animated.View style={{ opacity: this.dislikeOpacity, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
-            </Animated.View>
-
-            <Image
-              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-              source={item.uri} />
-
-          </Animated.View>
-        )
-      }
-      else {
-        return (
-          <Animated.View
-
-            key={item.id} style={[{
-              opacity: this.nextCardOpacity,
-              transform: [{ scale: this.nextCardScale }],
-              height: 400, width: 250, padding: 10, position: 'absolute'
-            }]}>
-            <Animated.View style={{ opacity: 0, transform: [{ rotate: '-30deg' }], position: 'absolute', top: 50, left: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'green', color: 'green', fontSize: 32, fontWeight: '800', padding: 10 }}>LIKE</Text>
-
-            </Animated.View>
-
-            <Animated.View style={{ opacity: 0, transform: [{ rotate: '30deg' }], position: 'absolute', top: 50, right: 40, zIndex: 1000 }}>
-              <Text style={{ borderWidth: 1, borderColor: 'red', color: 'red', fontSize: 32, fontWeight: '800', padding: 10 }}>NOPE</Text>
-
-            </Animated.View>
-
-            <Image
-              style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
-              source={item.uri} />
-
-          </Animated.View>
-        )
-      }
-    }).reverse()
-  }
-
   render() {
     return (
-      <Swiper cardsData={Users} renderCard={this._renderCard} 
-      style={{margin: 50,
-        backgroundColor: 'white',
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 5,}}/>
+      <Swiper
+        cardsData={Data}
+        renderCard={this._renderCard}
+        visibleDeckSize={2}
+        offSetAngleMin={-3}
+        offSetAngleMax={3}
+        velocityThreshold={0.4}
+        containerStyle={{
+          display: 'flex',
+          alignItems: 'center',
+          // justifyContent: 'center', //TODO
+        }}
+        style={{
+          margin: 20,
+          backgroundColor: 'white',
+          borderColor: 'black',
+          borderWidth: 1,
+          borderRadius: 5,
+
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- example/swiper.js
+++ example/swiper.js
@@ -1,9 +1,9 @@
+'use strict'
 import React from 'react'
-import { StyleSheet, Text, View, Dimensions, Animated, PanResponder } from 'react-native';
+import { StyleSheet, View, Animated, PanResponder } from 'react-native';
 import PropTypes from 'prop-types'
 
-const SCREEN_WIDTH = Dimensions.get('window').width
-const SCREEN_HEIGHT = Dimensions.get('window').height
+const ROTATION_MAGNITUDE = 25000 * 16
 
 export default class Swiper extends React.Component {
   constructor(props) {
@@ -17,13 +17,11 @@ export default class Swiper extends React.Component {
     this.position = new Animated.ValueXY({ x: 0, y: 0 })
     this.rotationTopCard = new Animated.Value(0)
     this.rotationBottomCard = new Animated.Value(0)
-    this.rotationBottomCardValue = null
-    this.rotationBottomCard.addListener(({ value }) => this.rotationBottomCardValue = value)
     this.cardOffsets = []
 
     this.rotateTop = this.rotationTopCard.interpolate({
-      inputRange: [-100000, 0, 100000],
-      outputRange: ['-150deg', '0deg', '150deg'],
+      inputRange: [-ROTATION_MAGNITUDE, 0, ROTATION_MAGNITUDE],
+      outputRange: ['-360deg', '0deg', '360deg'],
     })
     this.rotateBottom = this.rotationBottomCard.interpolate({
       inputRange: [-4, 0, 4],
@@ -40,26 +38,25 @@ export default class Swiper extends React.Component {
     this.bottomTransform = {
       transform: [{ rotate: this.rotateBottom }]
     }
-
-
   }
   componentDidMount() {
     //get initial offset angles for the visible deck
     const { offsetAngleMin, offsetAngleMax } = this.props
     this.cardOffsets = this.getInitialOffsets(offsetAngleMin, offsetAngleMax)
-    this.rotationTopCard.setValue(this.cardOffsets[0] * 100000 / 150)
-    this.rotationBottomCard.setValue(this.cardOffsets[2])
+
+    this.rotationTopCard.setValue(this.getInterpolatedRotation(this.cardOffsets[0]))
+    this.rotationBottomCard.setValue(this.cardOffsets[this.cardOffsets.length - 2])
 
   }
-  componentWillUnmount() {
-    this.rotationBottomCard.removeAllListeners()
-  }
+
   static defaultProps = {
     offsetAngleMin: -4,
     offsetAngleMax: 4,
     visibleDeckSize: 3,
     startIndex: 0,
+    velocityThreshold: 0.4
   }
+
   initializePanResponder = () => {
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (e, gestureState) => true,
@@ -77,52 +74,81 @@ export default class Swiper extends React.Component {
       onPanResponderRelease: (e, gestureState) => {
         const { moveX, moveY, dx, dy, vx, vy } = gestureState
         const { cardCenter, currentIndex } = this.state
-        const { cardsData } = this.props
+        const { cardsData, velocityThreshold } = this.props
+
         let x = moveX - cardCenter.x
         let y = moveY - cardCenter.y
 
         let rotation0 = x * dy - y * dx
         let rotation1000 = x * vy * 1000 - y * vx * 100
+
+        const finalPosition = { x: vx * 1000, y: vy * 1000 }
+        const finalRotation = rotation0 + rotation1000
+
         const vMagnitude = Math.sqrt(vx * vx + vy * vy)
-        if (vMagnitude > .4) {
-          Animated.parallel([
-            Animated.timing(this.position, {
-              toValue: { x: vx * 1000, y: vy * 1000 },
-              duration: 1000
-            }),
-            Animated.timing(this.rotationTopCard, {
-              toValue: rotation0 + rotation1000,
-              duration: 1000
-            })
-          ]
-          ).start(() => {
-            this.onSwipe(currentIndex, cardsData)
-          })
+        if (vMagnitude > velocityThreshold) {
+          this.animateCardOffScreen(finalPosition, finalRotation,
+            () => this.onSwipe(currentIndex, cardsData)
+          )
         }
         else {
-          Animated.spring(this.rotationTopCard, {
-            toValue: this.getInterpolatedRotation(this.cardOffsets[0]),
-            stiffness: 50,
-            damping: 30,
-            mass: 0.5
-          }).start()
-          Animated.spring(this.position, {
-            toValue: { x: 0, y: 0 },
-            friction: 4
-          }).start()
+          this.animateReset()
         }
       },
     })
   }
-
+  animateCardOffScreen = (finalPosition, finalRotation, cb) => {
+    Animated.parallel([
+      Animated.timing(this.position, {
+        toValue: finalPosition,
+        duration: 1000
+      }),
+      Animated.timing(this.rotationTopCard, {
+        toValue: finalRotation,
+        duration: 1000
+      })
+    ]
+    ).start(() => {
+      cb()
+    })
+  }
+  animateReset = () => {
+    Animated.spring(this.rotationTopCard, {
+      toValue: this.getInterpolatedRotation(this.cardOffsets[0]),
+      stiffness: 50,
+      damping: 30,
+      mass: 0.5
+    }).start()
+    Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 },
+      friction: 4
+    }).start()
+  }
   measureAnimatedView = (event) => {
     const { x, y, width, height } = event.nativeEvent.layout
     const cardCenter = { x: x + width / 2, y: y + height / 2 }
     if (!this.state.cardCenter || cardCenter.x !== this.state.cardCenter.x || cardCenter.y !== this.state.cardCenter.y)
       this.setState({
-        cardCenter
+        cardCenter,
       })
   }
+  getInitialOffsets = (min, max) => {
+    const { visibleDeckSize } = this.props
+    let offsets = []
+    //TODO
+    //O(n*m) where n is visibleDeckSize and m depends on probability of hitting a non-unique
+    //which gets higher as offsets.length approaches visibleDeckSize
+    //O(n^2)
+    //Sol: initialize map of all possible offsets 
+    //add to offsets from that map and remove from map
+    //shift offsets adds to the map
+    //O(n)
+    for (let i = 0; i < visibleDeckSize; i++) {
+      offsets.push(this.getUniqueElement(offsets, min, max))
+    }
+    return offsets
+  }
+
   //TODO
   //problems: uniques <= range(min, max)
   //if deckSize > range(min, max) there's not enough uniques
@@ -139,13 +165,30 @@ export default class Swiper extends React.Component {
     return [...offsets.slice(1), this.getUniqueElement(offsets.slice(1), offsetAngleMin, offsetAngleMax)]
   }
   getInterpolatedRotation = (deg) => {
-    return deg * 100000 / 150
+    return deg * ROTATION_MAGNITUDE / 360
   }
-  resetAnimatedValues = (x0, y0, rotation0) => {
+  resetTopCardAnimatedValues = (x0, y0, rotation0) => {
     this.position.setValue({ x: x0, y: y0 })
     this.rotationTopCard.setValue(rotation0)
+  }
+
+  onSwipe = (currentIndex, cardsData) => {
+    this.cardOffsets = this.updateCardOffsets(this.cardOffsets)
+    const topCardInitialRotation = this.getInterpolatedRotation(this.cardOffsets[0])
+    this.rotationBottomCard.setValue(this.cardOffsets[this.cardOffsets.length - 2])
+
+    if (currentIndex === cardsData.length - 1) {
+      this.setState({ currentIndex: 0 }, this.resetTopCardAnimatedValues(0, 0, topCardInitialRotation))
+    } else {
+      this.setState({ currentIndex: this.state.currentIndex + 1 }, this.resetTopCardAnimatedValues(0, 0, topCardInitialRotation))
+    }
+  }
 
 
+  getInitialRotation = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   animateBottomCard = (cb, value) => {
     Animated.timing(this.rotationBottomCard, {
@@ -153,42 +196,6 @@ export default class Swiper extends React.Component {
       duration: 500
     }).start(cb())
   }
-  onSwipe = (currentIndex, cardsData) => {
-    this.cardOffsets = this.updateCardOffsets(this.cardOffsets)
-    console.log('onswipe: ' + this.cardOffsets)
-    const topCardInitialRotation = this.getInterpolatedRotation(this.cardOffsets[0])
-    this.rotationBottomCard.setValue(this.cardOffsets[this.cardOffsets.length - 2])
-    console.log('onswipe rbc: ' + this.rotationBottomCardValue)
-
-    if (currentIndex === cardsData.length - 1) {
-      this.setState({ currentIndex: 0 }, this.resetAnimatedValues(0, 0, topCardInitialRotation))
-    } else {
-      this.setState({ currentIndex: this.state.currentIndex + 1 }, this.resetAnimatedValues(0, 0, topCardInitialRotation))
-    }
-  }
-
-  getInitialOffsets = (min, max) => {
-    const { visibleDeckSize } = this.props
-    let offsets = []
-    //TODO
-    //O(n*m) where n is visibleDeckSize and m depends on probability of hitting a non-unique
-    //which gets higher as offsets.length approaches visibleDeckSize
-    //O(n^2)
-    //Sol: initialize map of all possible offsets 
-    //add to offsets from that map and remove from map
-    //shift offsets adds to the map
-    //O(n)
-    for (i = 0; i < visibleDeckSize; i++) {
-      offsets.push(this.getUniqueElement(offsets, min, max))
-    }
-    return offsets
-  }
-  getInitialRotation = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   makeCard = (style, deckIndex, currentIndex, visibleDeckSize, renderCard, cardsData) => {
     let cardIndex = currentIndex + deckIndex
     if (cardIndex >= cardsData.length) {
@@ -196,17 +203,17 @@ export default class Swiper extends React.Component {
     }
     const isTopCard = deckIndex === 0
     const isLastCard = deckIndex === visibleDeckSize - 1
+    const transform = isTopCard
+      ? this.topTransform
+      : isLastCard
+        ? this.bottomTransform
+        : { transform: [{ rotate: `${this.cardOffsets[deckIndex]}deg` }] }
+
     if (isLastCard) {
-      // this.animateBottomCard(() => { },this.cardOffsets[deckIndex])
+      this.animateBottomCard(() => { }, this.cardOffsets[deckIndex])
     }
     return <Card
-      transform={
-        isTopCard
-          ? this.topTransform
-          : isLastCard
-            ? this.bottomTransform
-            : { transform: [{ rotate: `${this.cardOffsets[deckIndex]}deg` }] }
-      }
+      transform={transform}
       cardsData={cardsData}
       renderCard={renderCard}
       measureAnimatedView={this.measureAnimatedView}
@@ -219,25 +226,24 @@ export default class Swiper extends React.Component {
   }
   makeDeck = (style, currentIndex, visibleDeckSize, renderCard, cardsData) => {
     let deck = []
-    for (i = 0; i < visibleDeckSize; i++) {
+    for (let i = 0; i < visibleDeckSize; i++) {
       deck.push(this.makeCard(style, i, currentIndex, visibleDeckSize, renderCard, cardsData))
     }
     return deck
   }
-
   render() {
-    this.animateBottomCard(() => { },this.cardOffsets[2])
     const { currentIndex } = this.state
-    const { visibleDeckSize, renderCard, cardsData, style } = this.props
+    const { visibleDeckSize, renderCard, cardsData, style, containerStyle } = this.props
     return (
       <View
-        style={styles.container}
+        style={{ ...styles.container, ...containerStyle }}
       >
         {this.makeDeck(style, currentIndex, visibleDeckSize, renderCard, cardsData)}
       </View>
     )
   }
 }
+
 const Card = ({ style, panHandlers, deckIndex, transform, cardsData, cardIndex, renderCard, measureAnimatedView }) => {
   const _style = {
     ...style,
@@ -253,18 +259,19 @@ const Card = ({ style, panHandlers, deckIndex, transform, cardsData, cardIndex, 
     {renderCard(cardsData[cardIndex])}
   </Animated.View>
 }
+
 Swiper.propTypes = {
   cardsData: PropTypes.array.isRequired,
   renderCard: PropTypes.func.isRequired,
   visibleDeckSize: PropTypes.number,
   offsetAngleMax: PropTypes.number,
   startIndex: PropTypes.number,
+  velocityThreshold: PropTypes.number,
 }
 
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
-
   },
   card: {
     position: 'absolute',
